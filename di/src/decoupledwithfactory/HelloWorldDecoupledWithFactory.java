5bef763a5028c786636a0514f5bd599a759ed65d
BBK-PiJ-2015-87 <ivanovs223@gmail.com>
2016-01-19 16:05:19 +0000

    Trying invoke methods with reflections

--- di/src/decoupledwithfactory/HelloWorldDecoupledWithFactory.java
+++ di/src/decoupledwithfactory/HelloWorldDecoupledWithFactory.java
@@ -1,13 +1,30 @@
 package decoupledwithfactory;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class HelloWorldDecoupledWithFactory {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
         MessageRenderer mr = MessageSupportFactory.getInstance()
                 .getMessageRenderer();
         MessageProvider mp = MessageSupportFactory.getInstance()
                 .getMessageProvider();
         mr.setMessageProvider(mp);
         mr.render();
+
+//        Class clazz = Class.forName("decoupledwithfactory.CustomPhraseMessageProvider");
+//        Method m = clazz.getDeclaredMethod("getMessage");
+//        System.out.println(m.invoke(clazz.newInstance()));
+//
+//        CustomPhraseMessageProvider cmp = new CustomPhraseMessageProvider(){
+//            public String getMessage() {
+//                return "HELOOOOOOO";
+//            }
+//        };
+//
+//        Class clazz2 = cmp.getClass();
+//        Method m2 = clazz2.getDeclaredMethod("getMessage");
+//        System.out.println(m.invoke(new CustomPhraseMessageProvider()));
     }
 }
