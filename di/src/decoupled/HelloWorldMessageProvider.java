5418f80efef436be33cedcdeb16097b3473217c2
BBK-PiJ-2015-87 <ivanovs223@gmail.com>
2016-01-19 14:46:18 +0000

    Changing massage provider class

--- di/src/decoupled/HelloWorldMessageProvider.java
+++ di/src/decoupled/HelloWorldMessageProvider.java
@@ -1,7 +1,8 @@
 package decoupled;
 
 public class HelloWorldMessageProvider {
-	private String message = "Hello World!";
+//	private String message = "Hello World!";
+	private String message = "This can be changed!";
 
 	public String getMessage() {
 		return message;
